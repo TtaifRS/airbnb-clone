074afb9c39905687ccc7577e2d691b76e08fec37
Taif Rahaman <taif.rahaman73@gmail.com>
2023-04-05 00:44:41 +0600

    user date type to string

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,6 +1,5 @@
 'use client';
 import { useCallback, useState } from 'react';
-import {User} from '@prisma/client'
 import {AiOutlineMenu} from 'react-icons/ai'
 import {signOut} from 'next-auth/react'
 
@@ -10,9 +9,10 @@ import Avatar from '../Avatar';
 import MenuItem from './MenuItem';
 import useRegisterModel from '@/app/hooks/useRegisterModal';
 import useLoginModal from '@/app/hooks/useLoginModal';
+import { SafeUser } from '@/app/types';
 
 interface UserMenuProps{
-  currentUser?: User | null
+  currentUser?: SafeUser | null
 }
 
 const UserMenu: React.FC<UserMenuProps> = ({currentUser}) => {
