3900b3c4bd24adc8ae1de56ad8ae131901bdb630
Taif Rahaman <taif.rahaman73@gmail.com>
2023-04-05 17:03:17 +0600

    google oauth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,7 +2,7 @@ import NextAuth, { AuthOptions } from 'next-auth';
 import bcrypt from 'bcrypt'
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import GithubProvider from 'next-auth/providers/github'
-import GoogleProvider from 'next-auth/providers/facebook'
+import GoogleProvider from 'next-auth/providers/google'
 import CredentialsProvider from 'next-auth/providers/credentials';
 
 import prisma from '@/app/libs/prismadb'
